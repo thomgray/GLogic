3d025153a0ad9969d4d70ced6ca07a084e0d3d3e
Thomas Gray <thomdikdave@ThomasGraysMBP.home>
2016-04-23 23:51:29 +0100

    Fixed deallocation & memory leakage

---
+++ GLogic/NSWeakArray.h
@@ -0,0 +1,13 @@
+//
+//  NSWeakArray.h
+//  GLogic
+//
+//  Created by Thomas Gray on 22/04/2016.
+//  Copyright © 2016 Thomas Gray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSWeakArray : NSArray
+
+@end
---
+++ GLogic/NSWeakArray.m
@@ -0,0 +1,13 @@
+//
+//  NSWeakArray.m
+//  GLogic
+//
+//  Created by Thomas Gray on 22/04/2016.
+//  Copyright © 2016 Thomas Gray. All rights reserved.
+//
+
+#import "NSWeakArray.h"
+
+@implementation NSWeakArray
+
+@end
